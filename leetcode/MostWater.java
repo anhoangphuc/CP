55f53c480c7a9cb6dddbafdf93b1daabe831dc49
anhaanh <hoangphucnb97@gmail.com>
2020-07-08 08:23:00 +0700

    Add codeforces

---
+++ leetcode/MostWater.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+public class MostWater {
+    private int res = 0;
+    public int maxArea(int[] height) {
+    }
+
+    public static void main(String[] args) {
+        var myClass = new MostWater();
+        System.out.println(myClass.maxArea(new int[] {1,8,6,2,5,4,8,3,7 }));
+    }
+}
---
+++ leetcode/PalindromeNumber.java
@@ -0,0 +1,4 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+    }
+}
