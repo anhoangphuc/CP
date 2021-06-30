f08aa42d50f798b336294f2c2ca189a083a7543e
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2021-06-30 08:45:47 +0700

    Add some problems

--- init/test.cpp
+++ init/test.cpp
@@ -28,30 +28,5 @@ void debug_quicksort(vector<int>& a, int l, int r) {
 }
 
 int main() {
-    freopen("test.out", "w", stdout);
-    vector<int> a;
-    vector<int> b;
-    int cnt = 0;
-    const int NUMBER_OF_TEST = 1000;
-    const int LENGTH_OF_ARRAY = 1000;
-    srand(time(NULL));
-    cout << rand() % 1000000 << endl;
-    for (int i = 1; i <= NUMBER_OF_TEST; i++) {
-        a.clear();
-        for (int j = 0; j < LENGTH_OF_ARRAY; j++)
-            a.push_back(rand() % 10000);
-        b.clear();
-        b.insert(b.begin(), a.begin(), a.end());
-        sort(b.begin(), b.end());
-        debug_quicksort(a, 0, a.size());
-
-        if (a != b) {
-            for (auto x: a) 
-                cout << x << " ";
-            return 0;
-        } else {
-            cnt++;
-        }
-    }
-    cout << cnt;
+    cout << (-6 % 5);
 }
